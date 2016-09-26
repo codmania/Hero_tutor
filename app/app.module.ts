50a5f9fee7a564d284ad0376674c13b4eba47b78
Erwn <erwinbakkerwin@gmail.com>
2016-09-26 03:39:05 +0200

    finish routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,26 +1,30 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
 import { routing } from './app.routing';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        routing
-    ],
-    declarations: [
-        AppComponent,
-        DashboardComponent,
-        HeroDetailComponent,
-        HeroesComponent
-    ],
-    bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
-
-export class AppModule { }
+export class AppModule {
+}
